9e092120deb1d29cf0334da3b33186a115274169
Islam-Khairy <islamkhairy12@gmail.com>
2024-11-11 18:07:47 +0200

    features/accountantPage

--- src/app/[locale]/(website)/page.tsx
+++ src/app/[locale]/(website)/page.tsx
@@ -1,34 +1,34 @@
-import { Background } from "@/components/background/Background";
-import { AnalyticsStats } from "@/components/home/analytics/analytics";
-import { Faqs } from "@/components/home/faqs/Faqs";
-import { AboutUs } from "@/components/home/aboutUs/AboutUs";
-import { TeamWork } from "@/components/home/teamWork/TeamWork";
-import { Blogs } from "@/components/home/blogs/Blogs";
-import { Steps } from "@/components/home/steps/Steps";
-import { StepsChart } from "@/components/home/stepsChart/StepsChart";
-import { TopInstructors } from "@/components/home/topInstructors/TopInstructors";
-import { TopCourses } from "@/components/home/topCourses/TopCourses";
-import { Testimonial } from "@/components/home/testimonials/Testimonial"; // Updated path
-import NavigationControls from "@/components/home/navigationControls/NavigationControls";
-import { Accountants } from "@/components/home/accountants/Accountants";
+import { Background } from '@/components/background/Background';
+import { AnalyticsStats } from '@/components/home/analytics/analytics';
+import { Faqs } from '@/components/home/faqs/Faqs';
+import { AboutUs } from '@/components/home/aboutUs/AboutUs';
+import { TeamWork } from '@/components/home/teamWork/TeamWork';
+import { Blogs } from '@/components/home/blogs/Blogs';
+import { Steps } from '@/components/home/steps/Steps';
+import { StepsChart } from '@/components/home/stepsChart/StepsChart';
+import { TopInstructors } from '@/components/home/topInstructors/TopInstructors';
+import { TopCourses } from '@/components/home/topCourses/TopCourses';
+import { Testimonial } from '@/components/home/testimonials/Testimonial'; // Updated path
+import NavigationControls from '@/components/home/navigationControls/NavigationControls';
+import { AccountantSection } from '@/components/accountantSection/AccountantSection';
 
 export default async function Home() {
   return (
     <main
-      className="
+      className='
     bg-gradient-to-r
     from-white
     via-[#22b8dd86]
     to-white
-    background-animate"
+    background-animate'
     >
       {/* <CustomerService /> */}
       <Background />
-      <div className="md:container mx-auto z-0">
+      <div className='md:container mx-auto z-0'>
         <AnalyticsStats />
         <Steps />
         <StepsChart />
-        <Accountants />
+        <AccountantSection />
         <AboutUs />
         <TeamWork />
         <Testimonial />
