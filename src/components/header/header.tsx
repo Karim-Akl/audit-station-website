4325a047efca80d95cef42b3bd5f7acdfc402619
Islam-Khairy <islamkhairy12@gmail.com>
2024-11-17 18:00:26 +0200

    features/settings

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,98 +1,133 @@
-"use client";
-import React from "react";
-import { FaMapLocationDot } from "react-icons/fa6";
-import { MdOutlineMail } from "react-icons/md";
-import { CiFacebook, CiInstagram, CiLinkedin } from "react-icons/ci";
-import { FaXTwitter } from "react-icons/fa6";
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { FaMapLocationDot } from 'react-icons/fa6';
+import { MdOutlineMail } from 'react-icons/md';
+import { CiFacebook, CiLinkedin, CiYoutube } from 'react-icons/ci';
+import { RiTiktokLine } from "react-icons/ri";
+import { FaSnapchat } from "react-icons/fa6";
+
 import {
   Select,
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select";
-import { cn } from "@/lib/utils";
-import UseSearchParamsHook from "@/hooks/UseSearchParamsHook";
-import { useTheme } from "next-themes";
+} from '@/components/ui/select';
+import { cn } from '@/lib/utils';
+import UseSearchParamsHook from '@/hooks/UseSearchParamsHook';
+import { useTheme } from 'next-themes';
+import axios from 'axios';
+import { BASE_URL } from '@/lib/actions/actions';
+import Link from 'next/link';
+
 export const Header: React.FC = () => {
-  const { pathname, router } = UseSearchParamsHook();
-  const {theme} = useTheme();
-  const handlechangeLanguage = (value: string) => {
+  const [settings, setSettings] = useState<any>(null);
+  const { router } = UseSearchParamsHook();
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    const fetchSettings = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/api/public/settings`);
+        setSettings(response?.data?.data);
+      } catch (error) {
+        console.error('Error fetching settings:', error);
+      }
+    };
+    fetchSettings();
+  }, []);
+
+  const handleChangeLanguage = (value: string) => {
     router.push(`/${value}`);
   };
-  const handlechangeCurrency = (value: string) => {
-    console.log(value);
+
+  const handleChangeCurrency = (value: string) => {
+    console.log('Currency changed to:', value);
   };
+
+  if (!settings) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className={` w-full ${theme == "dark" ? "bg-[#1A1A1A]" : "bg-[#eee]"}`}>
-    <div className="container w-full flex-col justify-center items-start gap-2.5">
-      <div className="md:container self-stretch  md:justify-between items-center md:inline-flex">
-        <div className="justify-start items-center gap-8 flex ">
-          <div className="md:justify-start justify-center items-center gap-1 flex">
-            <FaMapLocationDot size={20} />
-            <div className=" md:text-[13px] text-[10px] md:font-medium ">
-              ul. Skrajna 7, Szczecin 70-886
+    <div className={`w-full ${theme === 'dark' ? 'bg-[#1A1A1A]' : 'bg-[#eee]'}`}>
+      <div className='container w-full flex-col justify-center items-start gap-2.5'>
+        <div className='md:container self-stretch md:justify-between items-center md:inline-flex'>
+          <div className='justify-start items-center gap-8 flex'>
+            <div className='md:justify-start justify-center items-center gap-1 flex'>
+              <FaMapLocationDot size={20} />
+              <div className='md:text-[13px] text-[10px] md:font-medium'>{settings?.address}</div>
+            </div>
+            <div className='md:justify-start justify-center items-center gap-1 flex'>
+              <MdOutlineMail size={20} />
+              <div className='md:text-[13px] text-[10px] md:font-medium'>{settings?.email}</div>
             </div>
           </div>
-          <div className="md:justify-start justify-center items-center gap-1 flex">
-            <MdOutlineMail size={20} />
-            <div className=" md:text-[13px] text-[10px] md:font-medium ">
-              alma.lawson@example.com
+          <div className='md:justify-start justify-around items-center md:gap-12 md:flex hidden'>
+            <div className='md:justify-start justify-center items-center md:gap-2 md:flex'>
+              <div className='text-[13px] font-medium font-sans'>Follow Us On :</div>
+              <div className='justify-start items-center gap-1 flex'>
+                <Link href={settings ? settings.facebook : ''}>
+                  <CiFacebook size={24} />
+                </Link>
+                <Link href={settings ? settings.linkedin : ''}>
+                  <CiLinkedin size={24} />
+                </Link>
+                <Link href={settings ? settings.tiktok : ''}>
+                  <RiTiktokLine size={24} />
+                </Link>
+                <Link href={settings ? settings.snapchat : ''}>
+                  <FaSnapchat size={24} />
+                </Link>
+                <Link href={settings ? settings.youtube : ''}>
+                  <CiYoutube size={24} />
+                </Link>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="md:justify-start justify-around  items-center md:gap-12 md:flex hidden">
-          <div className="md:justify-start justify-center items-center  md:gap-2 md:flex">
-            <div className=" text-[13px] font-medium font-sans">Follow Us On :</div>
-            <div className="justify-start  items-center gap-1 flex">
-              <CiFacebook size={24} />
-              <CiLinkedin size={24} />
-              <CiInstagram size={24} />
-              <FaXTwitter size={24} />
-            </div>
-          </div>
-          <div className="md:justify-start justify-center  items-center gap-4 flex">
-            <div className="justify-start items-center gap-2 flex border-e   ">
-              <Select onValueChange={handlechangeLanguage} defaultValue="en">
-                <SelectTrigger
-                  className={cn(
-                    "bg-blue border-none focus:ring-offset-0 focus:ring-0    "
-                  )}
+            <div className='md:justify-start justify-center items-center gap-4 flex'>
+              <div className='justify-start items-center gap-2 flex border-e'>
+                <Select
+                  onValueChange={handleChangeLanguage}
+                  defaultValue='en'
                 >
-                  <SelectValue />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectItem value="en">English</SelectItem>
-                    <SelectItem value="ar">Arabic</SelectItem>
-                    <SelectItem value="fr">French</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-            <div className="justify-start items-center gap-2 flex">
-              <Select onValueChange={handlechangeCurrency} defaultValue="USD">
-                <SelectTrigger
-                  className={cn(
-                    "bg-blue border-none focus:ring-offset-0 focus:ring-0    "
-                  )}
+                  <SelectTrigger
+                    className={cn('bg-blue border-none focus:ring-offset-0 focus:ring-0')}
+                  >
+                    <SelectValue />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value='en'>English</SelectItem>
+                      <SelectItem value='ar'>Arabic</SelectItem>
+                      <SelectItem value='fr'>French</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className='justify-start items-center gap-2 flex'>
+                <Select
+                  onValueChange={handleChangeCurrency}
+                  defaultValue='USD'
                 >
-                  <SelectValue />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectItem value="USD">$-USD</SelectItem>
-                    <SelectItem value="EGY">$-Egy</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
+                  <SelectTrigger
+                    className={cn('bg-blue border-none focus:ring-offset-0 focus:ring-0')}
+                  >
+                    <SelectValue />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value='USD'>$-USD</SelectItem>
+                      <SelectItem value='EGY'>$-Egy</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
--- src/components/home/steps/Steps.tsx
+++ src/components/home/steps/Steps.tsx
@@ -1,47 +1,30 @@
-import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
-import React, { useEffect, useState } from "react";
-import FristCard from "@../../../public/assets/Icon1.svg";
-import SecCard from "@../../../public/assets/Icon2.svg";
-import ThirdCard from "@../../../public/assets/Icon3.svg";
-import { getLocale } from "next-intl/server";
+import { InfiniteMovingCards } from '@/components/ui/infinite-moving-cards';
+import React, { useEffect, useState } from 'react';
+import FristCard from '@../../../public/assets/Icon1.svg';
+import SecCard from '@../../../public/assets/Icon2.svg';
+import ThirdCard from '@../../../public/assets/Icon3.svg';
+import { getLocale } from 'next-intl/server';
+import { fetchData } from '@/lib/api/fetchData';
 
 export async function Steps() {
+  const data = await fetchData({ endPoint: `/api/public/steps` });
   const locale = await getLocale();
   return (
-    <div className="h-[40rem] w-full rounded-md flex flex-col antialiased   dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
-      <div className="mb-8 text-center">
-        <h2 className="text-xl md:text-3xl font-bold text-gray-800">
-          Your steps to be <span className="text-blue-500">unique</span>
+    <div className='h-[40rem] w-full rounded-md flex flex-col antialiased   dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden'>
+      <div className='mb-8 text-center'>
+        <h2 className='text-xl md:text-3xl font-bold text-gray-800'>
+          Your steps to be <span className='text-blue-500'>unique</span>
         </h2>
-        <p className="text-gray-500 mt-2 md:max-w-2xl mx-auto">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Felis donec
-          massa at risus, Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit.
+        <p className='text-gray-500 mt-2 md:max-w-2xl mx-auto'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Felis donec massa at risus, Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit.
         </p>
       </div>
       <InfiniteMovingCards
-        items={testimonials}
-        direction={`${locale == "ar" ? "left" : "right"}`}
-        speed="slow"
+        items={data}
+        direction={`${locale == 'ar' ? 'left' : 'right'}`}
+        speed='slow'
       />
     </div>
   );
 }
-
-const testimonials = [
-  {
-    image: FristCard,
-    name: " Develop your skills through the courses on the platform!",
-    title: "01.",
-  },
-  {
-    image: SecCard,
-    name: " Develop your skills through the courses on the platform!",
-    title: "02.",
-  },
-  {
-    image: ThirdCard,
-    name: " Develop your skills through the courses on the platform!",
-    title: "03.",
-  },
-];
