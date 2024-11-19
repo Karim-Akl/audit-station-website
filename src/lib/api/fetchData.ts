a0d6940417336f34b31fd311d88329344f6d5843
Padel77 <adelassem99@gmail.com>
2024-11-20 00:30:40 +0200

    pull from  main

--- src/lib/api/fetchData.ts
+++ src/lib/api/fetchData.ts
@@ -1,6 +1,16 @@
-import { BASE_URL } from '@/lib/actions/actions';
+import { BASE_URL } from "@/lib/actions/actions";
 
-export async function fetchData({ endPoint, page, perPage, search }: { endPoint: string, page?: number, perPage?: number, search?: string }) {
+export async function fetchData({
+  endPoint,
+  page,
+  perPage,
+  search,
+}: {
+  endPoint: string;
+  page?: number;
+  perPage?: number;
+  search?: string;
+}) {
   const params = {
     page,
     per_page: perPage,
@@ -10,16 +20,18 @@ export async function fetchData({ endPoint, page, perPage, search }: { endPoint:
   const filteredParams = Object.fromEntries(
     Object.entries(params)
       .filter(([_, value]) => value !== undefined)
-      .map(([key, value]) => [key, String(value)]),
+      .map(([key, value]) => [key, String(value)])
   );
 
   const queryString = new URLSearchParams(filteredParams).toString();
 
   const response = await fetch(`${BASE_URL}${endPoint}?${queryString}`);
 
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data from ${endPoint}. Status: ${response.status}`);
-    }
-    const data = await response.json();
-    return data?.data
+  if (!response.ok) {
+    throw new Error(
+      `Failed to fetch data from ${endPoint}. Status: ${response.status}`
+    );
+  }
+  const data = await response.json();
+  return data?.data;
 }
