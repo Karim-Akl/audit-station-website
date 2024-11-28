898e03761f3a3d7614af55d0d5b9cdf1c3234fcb
Islam-Khairy <islamkhairy12@gmail.com>
2024-11-28 14:07:39 +0200

    new production

--- src/app/[locale]/(website)/blogs/all-blogs/page.tsx
+++ src/app/[locale]/(website)/blogs/all-blogs/page.tsx
@@ -2,11 +2,10 @@ import BlogsComponent from '@/components/blogs/BlogsComponent';
 import React from 'react';
 
 export const metadata = {
-  title: 'All Blogs',
+  title: 'All Blogss',
   description: 'Blogs Page',
 };
 export default async function AllBlogs() {
-
   return (
     <>
       <BlogsComponent />
